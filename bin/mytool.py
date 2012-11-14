4c65df90b6b7a411a9038e4033112280add4105a
jzou <jzou@opentext.com>
2012-11-14 18:24:17 -0500

    add encoding in loadBS

--- bin/mytool.py
+++ bin/mytool.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 import codecs
 
-def loadBS(name):
-    with codecs.open(name, 'r', 'utf-8') as f:
+def loadBS(name, encoding = 'utf-8'):
+    with codecs.open(name, 'r', encoding) as f:
         return BeautifulSoup(f)
 
 def getfile(name):
